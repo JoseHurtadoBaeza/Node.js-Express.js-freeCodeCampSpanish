2d70cb1285410aad9e2bc8677ca53a207e9dfb08
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-09-02 02:54:03 +0200

    First steps with Express

--- express-course/app.js
+++ express-course/app.js
@@ -4,3 +4,15 @@ const app = express();
 const {coursesInfo} = require('./courses.js');
 
 console.log(coursesInfo);
+
+// Routing
+
+app.get('/', (req, res) => {
+    res.send('My first server with Express. Courses 💻.');
+});
+
+const PORT = process.env.PORT || 3000; // The first option is for the hosting port provided
+
+app.listen(PORT, () => {
+    console.log(`The server is listening in the port ${PORT}`);
+});
